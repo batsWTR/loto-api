706fe22191b0037351f8ab11e9b7874a26a9f269
batsWTR <baptiste.wentzler@wanadoo.fr>
2022-03-30 13:35:38 +0200

    connection mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,25 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
-app.use((req,res) =>{
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1vwlg.mongodb.net/${process.env.DB_DB}?retryWrites=true&w=majority`,
+{ useNewUrlParser: true,
+  useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.get('/api', (req,res) =>{
     res.json({msg: 'le serveur fonctionne'});
 });
 
