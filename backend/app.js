08d29f81b8598616222a759ec733071bd5bbfbf6
batsWTR <baptiste.wentzler@wanadoo.fr>
2022-03-30 16:19:34 +0200

    ajout router

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const result = require('./models/result')
-
-
-const app = express();
-
-const fieldSelect = {
-  _id: 1, jour_de_tirage: 1,
-  date_de_tirage: 1,
-  combinaison_gagnante_en_ordre_croissant: 1,
-  combinaison_gagnant_second_tirage_en_ordre_croissant: 1
-};
+const resultRoute = require('./routes/result');
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1vwlg.mongodb.net/${process.env.DB_DB}?retryWrites=true&w=majority`,
 { useNewUrlParser: true,
@@ -20,7 +10,7 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cl
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-
+const app = express();
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -28,20 +18,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+app.use('/api', resultRoute);
 
-app.get('/api/getAll', (req,res) =>{
-    result.find()
-    .select(fieldSelect)
-    .then(results => res.status(200).json(results))
-    .catch(err => res.status(400).json(  {err}  ));
-});
 
-app.get('/api/getDay/:day', (req,res) =>{
-  console.log(req.params.day);
-  
-
-  res.send();
-});
 
 
 module.exports = app;
